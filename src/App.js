408eb7d32ab609f7f1700f8e3209e407eac96a34
LeeSeungHun <dos07008@naver.com>
2022-04-28 00:05:59 +0900

    1.button

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { Button } from "./components/Button";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Button size={"lg"} buttonValue={"lg"} />
+      <Button size={"md"} buttonValue={"md"} />
+      <Button size={"sm"} buttonValue={"sm"} />
+      <Button size={"xs"} buttonValue={"xs"} />
+      <hr></hr>
+      <Button size={"lg"} buttonValue={"lg"} state="disabled" />
+      <Button size={"md"} buttonValue={"md"} state="disabled" />
+      <Button size={"sm"} buttonValue={"sm"} state="disabled" />
+      <Button size={"xs"} buttonValue={"xs"} state="disabled" />
+    </>
   );
 }
 
