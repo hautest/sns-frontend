3ab6c7d8b8281c08b2dfe46ca3552747b06e4932
hautest <100465780+hautest@users.noreply.github.com>
2022-04-29 20:39:58 +0900

    버튼 개발 (#3)
    feat: button-component

---
+++ src/components/Button/constants.js
@@ -0,0 +1,24 @@
+import { css } from "styled-components";
+
+export const buttonSizes = {
+  lg: css`
+    padding: 10px 24px;
+    font-size: 18px;
+    line-height: 28px;
+  `,
+  md: css`
+    padding: 10px 16px;
+    font-size: 16px;
+    line-height: 24px;
+  `,
+  sm: css`
+    padding: 6px 12px;
+    font-size: 14px;
+    line-height: 20px;
+  `,
+  xs: css`
+    padding: 4px 8px;
+    font-size: 12px;
+    line-height: 16px;
+  `,
+};
---
+++ src/styles/theme.js
@@ -0,0 +1,18 @@
+const colors = {
+  black: "#000000",
+  gray1: "#333333",
+  gray2: "#4F4F4F",
+  gray3: "#828282",
+  gray4: "#BDBDBD",
+  gray5: "#E0E0E0",
+  gray6: "#F2F2F2",
+  primary: "#0085FF",
+  warn: "#EB5757",
+  white: "#ffffff",
+  success: "#219653",
+  hoverColor: "#0065c2",
+};
+
+export const theme = {
+  colors,
+};
