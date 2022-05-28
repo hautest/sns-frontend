6ec53104354bad3f4b07548a22d9b532d8187655
LeeSeungHun <dos07008@naver.com>
2022-05-28 16:53:31 +0900

    chore:recoil&react-query

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,22 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { Provider } from "react-redux";
+import { RecoilRoot } from "recoil";
+import { QueryClientProvider, QueryClient } from "react-query";
 import { store } from "./store";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as Element);
+
+const queryClient = new QueryClient();
+
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <RecoilRoot>
+      <QueryClientProvider client={queryClient}>
+        <Provider store={store}>
+          <App />
+        </Provider>
+      </QueryClientProvider>
+    </RecoilRoot>
   </React.StrictMode>
 );
